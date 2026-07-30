5328340fefdda7c641199d015009bd39176ac3de
xjflovelxy-sudo <xjflovelxy@gmail.com>
2026-07-30 23:06:17 +0800

    Add files via upload
    add sentence -level correction to all essay exercises

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "kaoyan-phrase-pwa-v12";
+const CACHE_VERSION = "kaoyan-phrase-pwa-v13";
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 

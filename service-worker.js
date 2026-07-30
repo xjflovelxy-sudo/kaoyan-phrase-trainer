09d95fff15185c9dddcb973e1e32c12669edabdb
xjflovelxy-sudo <xjflovelxy@gmail.com>
2026-07-30 11:14:00 +0800

    Add files via upload
    add  password reset support

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "kaoyan-phrase-pwa-v1";
+const CACHE_VERSION = "kaoyan-phrase-pwa-v2";
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 

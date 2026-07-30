95dda0d32bc461f3f6f07e5fdaabf5d48489a78c
xjflovelxy-sudo <xjflovelxy@gmail.com>
2026-07-30 17:14:01 +0800

    Add files via upload
    add full 144-day check-in calendar

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "kaoyan-phrase-pwa-v6";
+const CACHE_VERSION = "kaoyan-phrase-pwa-v7";
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 

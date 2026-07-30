6c68c97079354a37376e8f19a950430ca8726128
xjflovelxy-sudo <xjflovelxy@gmail.com>
2026-07-30 17:23:51 +0800

    Add files via upload
    redesign forward  check-in timeline

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "kaoyan-phrase-pwa-v7";
+const CACHE_VERSION = "kaoyan-phrase-pwa-v8";
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 

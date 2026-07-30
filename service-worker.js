1a0a8f0d5820ff395491a6c8ab7a6e4b135750e5
xjflovelxy-sudo <xjflovelxy@gmail.com>
2026-07-30 22:27:46 +0800

    Add files via upload
    fix  big  essay  writing practice and balance 300 tasks

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "kaoyan-phrase-pwa-v10";
+const CACHE_VERSION = "kaoyan-phrase-pwa-v11";
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 

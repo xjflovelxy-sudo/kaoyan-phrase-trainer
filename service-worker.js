7a511bf04edbd314989b4bc396975a153189b5a3
xjflovelxy-sudo <xjflovelxy@gmail.com>
2026-07-30 16:52:53 +0800

    Add files via upload
    add  check-in countdown and my marks center

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "kaoyan-phrase-pwa-v5";
+const CACHE_VERSION = "kaoyan-phrase-pwa-v6";
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 

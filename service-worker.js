28d8d66b5d6027b58c246d7a1f887a27c30c583a
xjflovelxy-sudo <xjflovelxy@gmail.com>
2026-07-31 12:14:55 +0800

    Add files via upload
    increase study plan to 90 minutes with core and bous goals

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "kaoyan-phrase-pwa-v17";
+const CACHE_VERSION = "kaoyan-phrase-pwa-v18";
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 

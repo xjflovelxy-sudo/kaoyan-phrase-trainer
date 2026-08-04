dd693b77ea7f78a5ca372c0f5651ccda3fc364f9
xjflovelxy-sudo <xjflovelxy@gmail.com>
2026-08-04 16:07:17 +0800

    Add files via upload
    Switch to Manual Check-in and Add Sentence Imitation Notes

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "kaoyan-phrase-pwa-v25";
+const CACHE_VERSION = "kaoyan-phrase-pwa-v26-manual-checkin";
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 

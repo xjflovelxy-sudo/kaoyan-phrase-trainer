7e463da08efc4e3e075f3ded99e09c0d93feb68f
xjflovelxy-sudo <xjflovelxy@gmail.com>
2026-08-10 00:00:07 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "kaoyan-phrase-pwa-v43-light-essay-year-progress";
+const CACHE_VERSION = "kaoyan-phrase-pwa-v43-unified-essay-light";
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 

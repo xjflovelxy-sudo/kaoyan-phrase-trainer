e34bd32fb986206cf5037099120d70b298f3ae4d
xjflovelxy-sudo <xjflovelxy@gmail.com>
2026-08-09 20:21:43 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "kaoyan-phrase-pwa-v39-safe-progress-fast-sync";
+const CACHE_VERSION = "kaoyan-phrase-pwa-v40-granular-storage-sync";
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 
